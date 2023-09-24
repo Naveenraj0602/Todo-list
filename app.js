513450c09929053cf1dc49c4e042892f21f006b9
naveen <naveenrajvimal@gmail.com>
2023-09-24 10:14:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// conenction to mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", { useNewUrlParser: true });
+
+
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+
+
+// routes
+app.use(require("./routes/index"))
+app.use(require("./routes/todo"))
+
+
+// server configurations....
+app.listen(3000, () => console.log("Server started listening on port: 3000"));
